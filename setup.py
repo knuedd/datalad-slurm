990aa364a57886f0b001235bef22a1ab7f8d80ff
Blablador <noreply@hzdr.de>
2026-06-29 10:23:42 +0200

    Switch to pyproject.toml-only config for PyPI compatibility

--- setup.py
+++ setup.py
@@ -1,50 +1,7 @@
 #!/usr/bin/env python
 
 import sys
-from setuptools import setup, find_packages
-import versioneer
+from setuptools import setup
 
-from _datalad_buildsupport.setup import (
-    BuildManPage,
-)
-
-cmdclass = versioneer.get_cmdclass()
-cmdclass.update(build_manpage=BuildManPage)
-
-if __name__ == '__main__':
-    setup(
-        name='datalad-slurm',
-        version=versioneer.get_version(),
-        packages=find_packages(where="src"),
-        package_dir={"": "src"},
-        cmdclass=cmdclass,
-        install_requires=[
-            "datalad>=0.18.0",
-            "sqlalchemy>=1.4.0",
-            "tqdm>=4.0.0",
-        ],
-        entry_points={
-            'datalad.extensions': [
-                'slurm=datalad_slurm:command_suite',
-            ],
-        },
-        python_requires=">=3.8",
-        author="Andreas Knüpfer, Timothy Callow",
-        author_email="a.knuepfer@hzdr.de, t.callow@hzdr.de",
-        description="A DataLad extension for HPC (slurm) systems",
-        long_description=open("README.md").read(),
-        long_description_content_type="text/markdown",
-        url="https://github.com/datalad/datalad-slurm",
-        license="MIT",
-        classifiers=[
-            "Development Status :: 3 - Alpha",
-            "Intended Audience :: Science/Research",
-            "License :: OSI Approved :: MIT License",
-            "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.8",
-            "Programming Language :: Python :: 3.9",
-            "Programming Language :: Python :: 3.10",
-            "Programming Language :: Python :: 3.11",
-            "Programming Language :: Python :: 3.12",
-        ],
-    )
+if __name__ == "__main__":
+    setup()
